59e4f9467844854d83f3182f76a0c41b6d566af8
dyerlab <dyerlab@icloud.com>
2024-02-12 12:11:21 -0500

    updating tests

--- Tests/PopGenTests/Array+FrequenciesTests.swift
+++ Tests/PopGenTests/Array+FrequenciesTests.swift
@@ -53,6 +53,23 @@ final class Array_FrequenciesTests: XCTestCase {
         XCTAssertEqual( X.colNames, ["A","B","C"] )
         
         print(X.description)
+        
+        XCTAssertEqual( X[0,0], freqs[0].forAllele(allele: "A") )
+        XCTAssertEqual( X[0,1], freqs[0].forAllele(allele: "B") )
+        XCTAssertEqual( X[0,2], freqs[0].forAllele(allele: "C") )
+        XCTAssertEqual( X[0,0] + X[0,1] + X[0,2], 1.0 )
+        XCTAssertEqual( X[1,0] + X[1,1] + X[1,2], 1.0 )
+        XCTAssertEqual( X[2,0] + X[2,1] + X[2,2], 1.0 )
+        
+        XCTAssertEqual( freqs[0].numHets, 3 )
+        XCTAssertEqual( freqs[1].numHets, 3 )
+        XCTAssertEqual( freqs[0].numDiploid, 7 )
+        XCTAssertEqual( freqs[0].forAllele(allele: "A"), 9.0/14.0 )
+        XCTAssertEqual( freqs[0].asDiversity.Ho, 3.0 / 7.0 )
+        XCTAssertEqual( freqs[0].asDiversity.He, 1.0 - ( (9.0/14.0 * 9.0/14.0) + (5.0/14.0 * 5.0/14.0) ) )
+        
+        
+        
 
     }
 
